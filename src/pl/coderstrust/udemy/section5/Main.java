9acabb245cf61a0e410bbd7793be42e5f40e5095
Tomek Wrobel <wrobelt1@gmail.com>
2018-12-19 22:00:56 +0100

    Initialize commit

---
+++ src/pl/coderstrust/udemy/section5/Main.java
@@ -0,0 +1,27 @@
+package pl.coderstrust.udemy.section5;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    printSquareStar(5);
+	    printSquareStar(12);
+    }
+    //komentarz
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        for (int i=0; i<number; i++) {
+
+            for (int j=0; j<number; j++) {
+                if (i == j || i == 0 || j == 0 || i == number-1 || j == number -1 || i == number- j-1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
